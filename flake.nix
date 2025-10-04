177df83a7db97d697f349eea1b590c19e08f61d1
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2025-10-04 22:41:43 +0800

    chore(dev): bump flake inputs, revisit flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,31 @@
 {
-  description = "The Negation Website";
+  description = "The Negation";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.hugo
-            pkgs.taplo
-            pkgs.nodejs_22
-            pkgs.vscode-langservers-extracted
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.hugo
+              pkgs.taplo
+              pkgs.nodejs_22
+              pkgs.vscode-langservers-extracted
+            ];
+          };
+        }
+      );
+    };
 }
